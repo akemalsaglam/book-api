7a3c8011e473ac2c2e0083b275c2f1adf52b0cb8
akemalsaglam <alikemalsaglam90@gmail.com>
2022-11-24 23:08:40 +0300

    add more test.

--- src/test/java/com/readingisgood/bookapi/domain/book/BookControllerTest.java
+++ src/test/java/com/readingisgood/bookapi/domain/book/BookControllerTest.java
@@ -1,6 +1,8 @@
 package com.readingisgood.bookapi.domain.book;
 
+import com.readingisgood.bookapi.domain.book.model.BookCreateRequest;
 import com.readingisgood.bookapi.domain.book.model.BookMapper;
+import com.readingisgood.bookapi.domain.book.model.BookRequest;
 import com.readingisgood.bookapi.domain.book.model.BookResponse;
 import com.readingisgood.bookapi.domain.common.controller.AbstractController;
 import com.readingisgood.bookapi.domain.common.exception.ResourceNotFoundException;
@@ -14,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -63,6 +66,22 @@ class BookControllerTest {
         final UUID bookId = UUID.randomUUID();
         when(bookService.findActiveById(bookId)).thenReturn(Optional.empty());
         Assertions.assertThrows(ResourceNotFoundException.class, () -> bookController.getBook(bookId));
+    }
+
+    @Test
+    void getBook_whenTryToInsertNewBook_ShouldReturnInsertedBook() {
+        final UUID bookId = UUID.randomUUID();
+        BookCreateRequest bookCreateRequest=new BookCreateRequest();
+        bookCreateRequest.setName("rEH9");
+        bookCreateRequest.setId(bookId);
+        bookCreateRequest.setIsbn("Ys3Dn");
+
+        final BookRequest bookRequest = BookMapper.INSTANCE.mapCreateRequestToRequest(bookCreateRequest);
+        final BookEntity bookEntity = BookMapper.INSTANCE.mapRequestToEntity(bookRequest);
+
+        when(bookService.save(any())).thenReturn(bookEntity);
+        final ResponseEntity<Object> bookResponse = bookController.createBook(bookCreateRequest);
+        Assertions.assertEquals(bookId, ((Optional<BookResponse>) bookResponse.getBody()).get().getId());
     }
 
 
